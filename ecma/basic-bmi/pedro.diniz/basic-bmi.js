c28ccb1a102b40074a66dfed1707c91d97df8edc
pedrold10 <pedrogabriel12423@gmail.com>
2020-03-02 14:24:45 -0300

    Ajeitando

--- ecma/basic-bmi/pedro.diniz/basic-bmi.js
+++ ecma/basic-bmi/pedro.diniz/basic-bmi.js
@@ -3,14 +3,14 @@ const h=1.72;
 let result;
 
 if((m/h**2)<18.5){
-    console.log('Você está abaixo do peso')
+    console.log('Você está abaixo do peso');
 }
 else if((m/h**2)<24.9){
-    console.log('Você está saudável')
+    console.log('Você está saudável');
 }
 else if((m/h**2)<29.9){
-    console.log('Você está acima do peso')
+    console.log('Você está acima do peso');
 }
 else{
-    console.log('Você está obeso')
+    console.log('Você está obeso');
 }
