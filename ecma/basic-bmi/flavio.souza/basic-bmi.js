6e85c3aef988023f9741f3ca545cc5e1132c9ae3
flaviodart <flaviohnrq879@gmail.com>
2020-03-03 12:12:00 -0300

    commit1

--- ecma/basic-bmi/flavio.souza/basic-bmi.js
+++ ecma/basic-bmi/flavio.souza/basic-bmi.js
@@ -3,12 +3,12 @@ let peso = 60;
 
 let bmiCalc = peso/altura ** 2;
 
-if (bmiCalc < 18.5){
+if (bmiCalc < 18.5) {
     console.log("Underweight")
 }   else if (bmiCalc >= 18.5){
     console.log("Normal Weigth")
 }   else if (bmiCalc >= 25){
     console.log("Overweight")
-}   else{
-    console.log("Savio!!!")
+}   else {
+    console.log("Obesity!")
 }
