8784f25b257e17171bc6fd04c4509bf10ea1f152
Aleczk <alecsandercamilo@gmail.com>
2020-03-03 19:05:54 -0300

    Fibbonnaci Number and Sequence

--- ecma/function-fibonacci-number/alecsander.camilo/function-fibonnaci-number.js
+++ ecma/function-fibonacci-number/alecsander.camilo/function-fibonnaci-number.js
@@ -1,23 +1,12 @@
 // Alecsander Camilo - 20192370010
 
-function fibonnaciNumber(x) {
-    
-    let number = [0, 1, 1, 2, 3, 5, 8]
+let number = [0, 1, 1, 2, 3, 5, 8]
 
+function fibonnaciNumber(x) {
     if (x === 0) {
         return 'undefined';
-    } else if (x === 1) {
-        return number[0];
-    } else if (x === 2) {
-        return number[1]
-    } else if (x === 3) {
-        return number[2];
-    } else if (x === 4) {
-        return number[3];
-    } else if (x === 5) {
-        return number[4];
-    } else if (x === 6) {
-        return number[5];
+    } else {
+        return number[x - 1];
     }
 }
 
---
+++ ecma/function-fibonacci-sequence/alecsander.camilo/function-fibonacci-sequence.js
@@ -0,0 +1,22 @@
+// Alecsander Camilo - 20192370010
+
+let fib = [0, 1]
+
+function fibonnaciSequence(x) {
+    if (x === 0) {
+        return '';
+    } else if (x === 1) {
+        return 0;
+    } else {
+        for (let number = 2; number <= x; number++) {
+            fib[number] = fib[number - 2] + fib[number - 1];
+    }  
+        return fib.slice(0, -1).join(', ');
+    }
+}
+
+console.log(fibonnaciSequence(0))
+console.log(fibonnaciSequence(1))
+console.log(fibonnaciSequence(2))
+console.log(fibonnaciSequence(4))
+console.log(fibonnaciSequence(6))
