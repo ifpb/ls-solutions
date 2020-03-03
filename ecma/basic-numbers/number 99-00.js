4f4499587c1e4c1c108b793e1e62eabcd68546a4
Jefferson-Marcelo <jeffersonmarcelo_os@hotmail.com>
2020-03-02 21:37:21 -0300

    Lista de exercícios de números

---
+++ ecma/basic-numbers/number 99-00.js
@@ -0,0 +1,7 @@
+var result=''
+for (var number = 99; number >= 0 ; number--){
+    result += number < 10 ? '0' + number : number;
+    result += number % 10 === 0 ? '\n' : ' ';
+}
+
+console.log(result)
