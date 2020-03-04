4ec67d1f2e372d01b63772dc7be5a9f80b6c79a1
lukkerr <luciano_carvalho@outlook.com.br>
2020-03-03 23:32:00 -0300

    Envio de Exercícios

---
+++ ecma/function-factorial/luciano.filho/factorial.print.js
@@ -0,0 +1,13 @@
+factorial = require("./factorial.js");
+
+console.log(factorial(1));
+console.log(1);
+
+console.log(factorial(2));
+console.log(2);
+
+console.log(factorial(3));
+console.log(6);
+
+console.log(factorial(4));
+console.log(24);
