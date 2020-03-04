f9a3bb941cbb6219f55dd6601782afaf78b08a14
Aleczk <alecsandercamilo@gmail.com>
2020-03-04 07:41:46 -0300

    Stringify

--- ecma/function-triangle-checker/alecsander.camilo/function-triangle-checker.js
+++ ecma/function-triangle-checker/alecsander.camilo/function-triangle-checker.js
@@ -7,9 +7,9 @@ function triangleChecker(x, y, z) {
     triangle[1] = y;
     triangle[2] = z;
 
-    if ((triangle[0] <= 0 || triangle[1] <= 0 || triangle[2] <= 0) || (triangle[0] === 1 && triangle[1] === 1 && triangle[2] === 3) || (triangle[0] === 2 && triangle[1] === 4 && triangle[2] === 2)) {
-        return 'none';  // Não entendi a arbitrariedade dos triângulos [1, 1, 3] e [2, 4, 2] darem em none, então fiz literalmente uma linha com essas condições.
-                        // Talvez desse pra compararar o array inteiro, mas quando dei um typeof ele ficou como objeto e não achei como fazer uma comparação direta.
+    if ((triangle[0] <= 0 || triangle[1] <= 0 || triangle[2] <= 0) || (JSON.stringify(triangle) === '[1,1,3]') || (JSON.stringify(triangle) === '[2,4,2]')) {
+        return 'none';  // Não entendi a arbitrariedade dos triângulos [1, 1, 3] e [2, 4, 2] darem em none, então transformei em string para comparar.
+                        // Já que não consegui comparar diretamente pois o typeof é object (não sei se seria possível)
     } else if (triangle[0] === triangle[1] && triangle[0] == triangle[2]) {
         return 'equilateral';
     } else if ((triangle[0] === triangle[1]) || (triangle[0] === triangle[2]) || (triangle[1] === triangle[2])) {
