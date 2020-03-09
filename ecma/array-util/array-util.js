b7f25fa353f38fd9f272b41dea39c1c71d069cc2
flaviodart <flaviohnrq879@gmail.com>
2020-03-09 14:01:57 -0300

    array util

---
+++ ecma/array-util/array-util.js
@@ -0,0 +1,38 @@
+function min(array) {
+  let minimun = array.shift();
+  for (const value of array){
+    if (value < minimun){
+      minimun = value
+    }
+  }
+  return minimun
+}
+
+function max(array) {
+  let maximun = array.shift();
+  for (const value of array){
+    if (value > maximun){
+      maximun = value
+    }
+  }
+  return maximun
+}
+}
+
+function range(length, last, step) {
+  // TODO
+}
+
+function zip(...arrays) {
+  // TODO
+}
+
+function uniq(array) {
+  // TODO
+}
+
+function sortNum(array) {
+  // TODO
+}
+
+export { min, max, range, zip, uniq, sortNum };
