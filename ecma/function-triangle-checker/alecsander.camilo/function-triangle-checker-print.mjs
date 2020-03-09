a63a3693181893eefa85228e6d9961fd61a1f777
Aleczk <alecsandercamilo@gmail.com>
2020-03-09 02:21:37 -0300

    Import/Export Functions added

---
+++ ecma/function-triangle-checker/alecsander.camilo/function-triangle-checker-print.mjs
@@ -0,0 +1,16 @@
+// Alecsander Camilo - 20192370010
+
+import { triangleChecker } from "./function-triangle-checker.mjs";
+
+console.log(triangleChecker(2, 2, 2));
+console.log(triangleChecker(10, 10, 10));
+console.log(triangleChecker(3, 4, 4));
+console.log(triangleChecker(4, 3, 4));
+console.log(triangleChecker(4, 4, 3));
+console.log(triangleChecker(3, 4, 5));
+console.log(triangleChecker(10, 11, 12));
+console.log(triangleChecker(5, 4, 2));
+console.log(triangleChecker(0, 0, 0));
+console.log(triangleChecker(3, 4, -5));
+console.log(triangleChecker(1, 1, 3));
+console.log(triangleChecker(2, 4, 2));
--- ecma/function-triangle-checker/alecsander.camilo/function-triangle-checker.mjs
+++ ecma/function-triangle-checker/alecsander.camilo/function-triangle-checker.mjs
@@ -19,15 +19,4 @@ function triangleChecker(x, y, z) {
     }
 }
 
-console.log(triangleChecker(2, 2, 2));
-console.log(triangleChecker(10, 10, 10));
-console.log(triangleChecker(3, 4, 4));
-console.log(triangleChecker(4, 3, 4));
-console.log(triangleChecker(4, 4, 3));
-console.log(triangleChecker(3, 4, 5));
-console.log(triangleChecker(10, 11, 12));
-console.log(triangleChecker(5, 4, 2));
-console.log(triangleChecker(0, 0, 0));
-console.log(triangleChecker(3, 4, -5));
-console.log(triangleChecker(1, 1, 3));
-console.log(triangleChecker(2, 4, 2));
+export { triangleChecker }
