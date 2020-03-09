b59e90463e9a87e2c91456d7e7fac3f0f180b4a5
lukkerr <luciano_carvalho@outlook.com.br>
2020-03-09 14:07:07 -0300

    Envio de Exercícios

---
+++ ecma/array-util/luciano.filho/array-util.js
@@ -0,0 +1,27 @@
+function min(array) {
+  return Math.min(...array);
+  // TODO
+}
+
+function max(array) {
+  return Math.max(...array);
+  // TODO
+}
+
+function range(length, last, step) {
+  // TODO
+}
+
+function zip(...arrays) {
+  // TODO
+}
+
+function uniq(array) {
+  // TODO
+}
+
+function sortNum(array) {
+  // TODO
+}
+
+export { min, max, range, zip, uniq, sortNum };
