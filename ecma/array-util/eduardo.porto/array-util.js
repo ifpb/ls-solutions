da276ea59a793cc63c7ebc86d17ee1b4c3521ff0
IFPB <ifpb@MAC14LB04-JP.local>
2020-03-09 14:38:08 -0300

    Exercicio de array

---
+++ ecma/array-util/eduardo.porto/array-util.js
@@ -0,0 +1,27 @@
+function min(array) {
+  // TODO
+  return Math.min(...array);
+}
+
+function max(array) {
+  // TODO
+  return Math.max(...array);
+}
+
+function range(length, last, step) {
+  // TODO
+}
+
+function zip(...arrays) {
+  // TODO
+}
+
+function uniq(array) {
+  // TODO
+}
+
+function sortNum(array) {
+  // TODO
+}
+
+export { min, max, range, zip, uniq, sortNum };
