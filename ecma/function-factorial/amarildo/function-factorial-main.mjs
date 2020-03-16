9767993c269ba53319099399d3444726048166b3
Amarildo <amarildop1dev@gmail.com>
2020-03-16 19:39:39 -0300

    Function Factorial - Exercise 09

--- ecma/function-factorial/amarildo/function-factorial-main.mjs
+++
@@ -1,8 +0,0 @@
-import { factorial } from "./function-factorial.mjs";
-
-console.log(factorial(1));
-console.log(factorial(2));
-console.log(factorial(3));
-console.log(factorial(4));
-console.log(factorial(5));
-console.log(factorial(6));
