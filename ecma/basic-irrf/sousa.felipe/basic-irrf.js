a458202b434e2afe5ec02813f713f8bd281bb0a9
felipeGsousa <felipe31galdino@gmail.com>
2020-03-17 22:00:23 -0300

    Correção no exercicio basic-irrf

--- ecma/basic-irrf/sousa.felipe/basic-irrf.js
+++ ecma/basic-irrf/sousa.felipe/basic-irrf.js
@@ -1,6 +1,5 @@
 const salario = 5000;
-let taxrate = 0;
-let deduction = 0;
+let taxRate = 0, deduction = 0;
 
 if (salario > 4664.68){
     taxRate = 0.275;
@@ -9,14 +8,14 @@ if (salario > 4664.68){
     taxRate = 0.225;
     deduction = 636.13;
 }else if (salario >= 2826.66){
-    taxRate = 0.15
-    deduction = 354.80
+    taxRate = 0.15;
+    deduction = 354.80;
 }else if (salario >= 1903.99){
-    taxRate = 0.075
-    deduction = 142.80
+    taxRate = 0.075;
+    deduction = 142.80;
 }
 
 
-tax = (salario * taxRate) - deduction
+let tax = (salario * taxRate) - deduction;
 
 console.log(`${tax}`)
