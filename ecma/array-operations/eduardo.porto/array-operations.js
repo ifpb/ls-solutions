e83dece288dbb7bc0c3c3710c3efbda563a61daf
DuduPorto2000 <dudu@porto.ws>
2020-03-30 14:54:49 -0300

    correção de exercicios

--- ecma/array-operations/eduardo.porto/array-operations.js
+++ ecma/array-operations/eduardo.porto/array-operations.js
@@ -1,13 +1,13 @@
 function sum(array) {
-  array.reduce((x,y) => x + y, 0);
+  return array.reduce((x,y) => x + y, 0);
 }
 
 function product(array) {
-  array.reduce((x,y) => x * y, 1);
+  return array.reduce((x,y) => x * y, 1);
 }
 
 function sumOdds(array) {
-  array.filter(x => x % 2 != 0).reduce((x,y) => x + y, 0);
+  return array.filter(x => x % 2 != 0).reduce((x,y) => x + y, 0);
 }
 
 export { sum, product, sumOdds };
