5816068262fd7ce22504e5d1f1b8fae85509c271
Amarildo <amarildop1dev@gmail.com>
2020-04-07 23:57:11 -0300

    Array Util

---
+++ ecma/array-util/amarildo.melo/array-util-main.mjs
@@ -0,0 +1,40 @@
+import { min, max, range, zip, uniq, sortNum } from "./array-util.mjs";
+
+// finding lowest value in [1, 4, 2, 6, 10, 3]
+console.log(min([1, 4, 2, 6, 10, 3]));
+
+// finding lowest value in [1, 4, -1, 6, 10, 3]
+console.log(min([1, 4, -1, 6, 10, 3]));
+
+
+/* 
+    // finding greatest value in [1, 4, 2, 6, 10, 3]
+    console.log(max([1, 4, 2, 6, 10, 3]));
+
+    // generating range of numbers from 0 to 10
+    console.log(range(10));
+
+    // generating range of numbers from 1 to 11
+    console.log(range(1, 11));
+
+    // generating range of numbers from 0 to 10 with steps
+    console.log(range(0, 30, 5));
+
+    // generating zip of [\'moe\', \'larry\'] and [30, 40]
+    console.log(zip(["moe", "larry"], [30, 40]));
+
+    // generating zip of [\'moe\', \'larry\', \'curly\'], [30, 40, 50] and [true, false, false]
+    console.log(zip(["moe", "larry", "curly"], [30, 40, 50], [true, false, false]));
+
+    // removing duplicate values in [1, 2, 1, 4, 1, 3]
+    console.log(uniq([1, 2, 1, 4, 1, 3]));
+
+    // removing duplicate values in [1, 2, 1, 3, 3]
+    console.log(uniq([1, 2, 1, 3, 3]));
+
+    // sorting number values in [1, 3, 2]
+    console.log(sortNum([1, 3, 2]));
+
+    // sorting number values in [1, 2, 10, 3, 32]
+    console.log(sortNum([1, 2, 10, 3, 32]));
+ */
