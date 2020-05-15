c29df814770e1ac8244a8f8d6dbcb6336e1ed9d8
Amarildo <amarildop1dev@gmail.com>
2020-05-15 20:25:08 -0300

    Exercise 30 - String Util - 37,5% completed

--- ecma/string-util/amarildo.melo/string-util-main.mjs
+++ ecma/string-util/amarildo.melo/string-util-main.mjs
@@ -4,3 +4,6 @@ import { formatter } from "./string-util.mjs";
 console.log(formatter("lorem ipsum dolor", "uppercase"));
 
 console.log(formatter("lorem ipsum dolor", "lowercase"));
+
+//Adiciona um '_' entre as palavras 
+console.log(formatter("lorem ipsum dolor", "snakecase"));
--- ecma/string-util/amarildo.melo/string-util.mjs
+++ ecma/string-util/amarildo.melo/string-util.mjs
@@ -1,11 +1,21 @@
+function snakeCase(texto){
+    let array = texto.split(' ');
+    let textoSnake = array.join('_');
+    return textoSnake;
+}
+
+
 function formatter(text, action){
     switch(action){
         case 'uppercase':
             return text.toUpperCase();
         case 'lowercase':
             return text.toLowerCase();
+        case 'snakecase':
+        	return snakeCase(text);
+
     }
 }
 
 
-export { formatter };
+export { formatter, snakeCase };
